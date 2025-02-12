949edbc267640d17d19ad9d9f764362efadf7058
lunarstonks <powerpixel20@gmail.com>
2025-02-12 20:45:35 +0100

    update

---
+++ src/main/java/it/jar1/event/TickListener.java
@@ -0,0 +1,13 @@
+package it.jar1.event;
+
+import org.bukkit.scheduler.*;
+
+public class TickListener extends BukkitRunnable
+{
+    public void run() {
+        this.onEvent();
+    }
+    
+    public void onEvent() {
+    }
+}
